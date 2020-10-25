26a8059d58efd25412612742677ddfd1ef81678a
ethanae <eemandien@mweb.co.za>
2020-10-25 15:54:25 +0200

    - add plugboard class

---
+++ python/plugboard.py
@@ -0,0 +1,12 @@
+class Plugboard:
+  def __init__(self, alphabet, output):
+    self.input = alphabet
+    self.output = output
+
+  def map(self, letter):
+    for i, c in self.input:
+      if c == letter:
+        return self.output[i]
+      if self.output[i] == letter:
+        return c
+    return letter
